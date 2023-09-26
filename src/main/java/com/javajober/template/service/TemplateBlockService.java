0fe055af2f60b673935ed7197ca6267b1eee1958
yunhyeonjin <h00j@naver.com>
2023-09-26 22:41:05 +0900

    chore: TemplateBlockService 분리(#37)

---
+++ src/main/java/com/javajober/template/service/TemplateBlockService.java
@@ -0,0 +1,76 @@
+package com.javajober.template.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javajober.addSpace.repository.AddSpaceRepository;
+import com.javajober.entity.MemberGroup;
+import com.javajober.entity.TemplateAuth;
+import com.javajober.entity.TemplateBlock;
+import com.javajober.template.dto.TemplateBlockRequest;
+import com.javajober.template.dto.TemplateBlockRequests;
+import com.javajober.template.dto.TemplateBlockResponse;
+import com.javajober.template.repository.MemberGroupRepository;
+import com.javajober.template.repository.SpaceWallCategoryRepository;
+import com.javajober.template.repository.TemplateAuthRepository;
+import com.javajober.template.repository.TemplateBlockRepository;
+import com.javajober.template.repository.TemplateRepository;
+
+@Service
+public class TemplateBlockService {
+
+	private final MemberGroupRepository memberGroupRepository;
+	private final TemplateAuthRepository templateAuthRepository;
+	private final TemplateBlockRepository templateBlockRepository;
+
+	public TemplateBlockService(MemberGroupRepository memberGroupRepository, TemplateAuthRepository templateAuthRepository, TemplateBlockRepository templateBlockRepository) {
+		this.memberGroupRepository = memberGroupRepository;
+		this.templateAuthRepository = templateAuthRepository;
+		this.templateBlockRepository = templateBlockRepository;
+	}
+
+	@Transactional
+	public void save(final TemplateBlockRequests<TemplateBlockRequest> templateBlockRequests){
+
+		List<TemplateBlockRequest> subDataList = templateBlockRequests.getSubData();
+
+		for (TemplateBlockRequest templateBlockRequest : subDataList) {
+
+			TemplateBlock templateBlock = TemplateBlockRequest.toEntity(templateBlockRequest);
+			templateBlockRepository.save(templateBlock);
+
+			List<Long> allAuthIds = templateBlockRequest.getAllAuthIds();
+
+			for (Long authId : allAuthIds) {
+				MemberGroup memberGroup = memberGroupRepository.getById(authId);
+				Boolean hasAccess = templateBlockRequest.getHasAccessTemplateAuth().contains(authId);
+				TemplateAuth templateAuth = new TemplateAuth(memberGroup, hasAccess, templateBlock);
+				templateAuthRepository.save(templateAuth);
+			}
+		}
+	}
+
+	@Transactional
+	public TemplateBlockResponse getTemplateBlock(Long templateBlockId){
+
+		TemplateBlock templateBlock = templateBlockRepository.getById(templateBlockId);
+
+		List<TemplateAuth> templateAuths = templateAuthRepository.findByTemplateBlockId(templateBlockId);
+
+		List<Long> hasAccessTemplateAuth = new ArrayList<>();
+		List<Long> hasDenyTemplateAuth = new ArrayList<>();
+
+		for (TemplateAuth auth : templateAuths) {
+			if (auth.getHasAccess()) {
+				hasAccessTemplateAuth.add(auth.getAuthMember().getId());
+			} else {
+				hasDenyTemplateAuth.add(auth.getAuthMember().getId());
+			}
+		}
+		return TemplateBlockResponse.from(templateBlock, hasAccessTemplateAuth, hasDenyTemplateAuth);
+	}
+
+}
